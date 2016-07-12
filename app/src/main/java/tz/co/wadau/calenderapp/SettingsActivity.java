311b65455d4db10c2fe2c59288fa4ab20656511e
Robby <0881londo@gmail.com>
2016-07-13 00:11:48 +0300

    update

--- app/src/main/java/tz/co/wadau/calenderapp/SettingsActivity.java
+++ app/src/main/java/tz/co/wadau/calenderapp/SettingsActivity.java
@@ -1,29 +1,53 @@
 package tz.co.wadau.calenderapp;
 
-import android.os.Bundle;
+import android.content.SharedPreferences;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
+import android.view.MenuItem;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends PreferenceActivity {
+    public static final String KEY_PREF_SYNC_CONN = "pref_syncConnectionType";
 
-    Toolbar toolbar;
-
-//    @Override
-    public void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_settings);
 
         // Display the fragment as the main content.
         getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
+                .replace(R.id.fragment_container, new SettingsFragment())
                 .commit();
     }
 
-//    protected void onPostCreate(Bundle savedInstanceState) {
-//        super.onPostCreate(savedInstanceState);
-//        getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
-//    }
+    SharedPreferences.OnSharedPreferenceChangeListener listener =
+            new SharedPreferences.OnSharedPreferenceChangeListener() {
+                public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+                    // listener implementation
+                    if (key.equals(KEY_PREF_SYNC_CONN)) {
+                        Preference connectionPref = Preference.findPreference(key);
+                        // Set summary to be the user-description for the selected value
+                        connectionPref.setSummary(sharedPreferences.getString(key, ""));
+                    }
+                }
+            };
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .registerOnSharedPreferenceChangeListener(listener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .unregisterOnSharedPreferenceChangeListener(listener);
+    }
+
 
 }
--- app/src/main/java/tz/co/wadau/calenderapp/SettingsFragment.java
+++ app/src/main/java/tz/co/wadau/calenderapp/SettingsFragment.java
@@ -1,9 +1,19 @@
 package tz.co.wadau.calenderapp;
 
+
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+/**
+ * A simple {@link Fragment} subclass.
+ */
 public class SettingsFragment extends PreferenceFragment {
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -11,4 +21,5 @@ public class SettingsFragment extends PreferenceFragment {
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
     }
+
 }
