c39a96a00b343e5e7163cbe9be508df7965afa94
Robby <0881londo@gmail.com>
2016-07-20 02:12:58 +0300

    update

--- app/src/main/java/tz/co/wadau/calenderapp/SettingsActivity.java
+++ app/src/main/java/tz/co/wadau/calenderapp/SettingsActivity.java
@@ -5,9 +5,10 @@ import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class SettingsActivity extends PreferenceActivity {
-    public static final String KEY_PREF_SYNC_CONN = "pref_syncConnectionType";
+public class SettingsActivity extends AppCompatActivity {
+//    public static final String KEY_PREF_SYNC_CONN = "pref_syncConnectionType";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,31 +21,21 @@ public class SettingsActivity extends PreferenceActivity {
                 .commit();
     }
 
-    SharedPreferences.OnSharedPreferenceChangeListener listener =
-            new SharedPreferences.OnSharedPreferenceChangeListener() {
-                public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
-                    // listener implementation
-                    if (key.equals(KEY_PREF_SYNC_CONN)) {
-                        Preference connectionPref = findPreference(key);
-                        // Set summary to be the user-description for the selected value
-                        connectionPref.setSummary(prefs.getString(key, ""));
-                    }
-                }
-            };
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        PreferenceManager.getDefaultSharedPreferences(this)
-                .registerOnSharedPreferenceChangeListener(listener);
-    }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        PreferenceManager.getDefaultSharedPreferences(this)
-                .unregisterOnSharedPreferenceChangeListener(listener);
-    }
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        PreferenceManager.getDefaultSharedPreferences(this)
+//                .registerOnSharedPreferenceChangeListener(listener);
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        PreferenceManager.getDefaultSharedPreferences(this)
+//                .unregisterOnSharedPreferenceChangeListener(listener);
+//    }
 
 
 }
--- app/src/main/java/tz/co/wadau/calenderapp/SettingsFragment.java
+++ app/src/main/java/tz/co/wadau/calenderapp/SettingsFragment.java
@@ -1,9 +1,13 @@
 package tz.co.wadau.calenderapp;
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +16,8 @@ import android.widget.TextView;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
+    public static final String KEY_PREF_EXERCISES = "prefs_mens_days";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,32 @@ public class SettingsFragment extends PreferenceFragment {
 
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key)
+    {
+        //IT NEVER GETS IN HERE!
+        if (key.equals(KEY_PREF_EXERCISES)) {
+            // Set summary to be the user-description for the selected value
+            Preference exercisesPref = findPreference(key);
+            exercisesPref.setSummary(sharedPreferences.getString(key, ""));
+
+        }
+        Log.d("SOME", key);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+        Log.d("SOME", "some");
+    }
+
+    @Override
+    public void onPause() {
+        getPreferenceManager().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+        super.onPause();
     }
 
 }
