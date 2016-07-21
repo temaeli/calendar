f7a5d5b64f43ccd0aa8c643e62fdc696a000b244
Robby <0881londo@gmail.com>
2016-07-22 00:52:41 +0300

    Adds custom NumberPickerPreference

--- app/src/main/java/tz/co/wadau/calenderapp/NumberPickerPreference.java
+++ app/src/main/java/tz/co/wadau/calenderapp/NumberPickerPreference.java
@@ -4,56 +4,90 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.preference.DialogPreference;
 import android.util.AttributeSet;
-import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.NumberPicker;
 
 
 public class NumberPickerPreference extends DialogPreference {
 
-    NumberPicker picker;
-    Integer initialValue;
+    public static final int DEFAULT_MAX_VALUE = 35;
+    public static final int DEFAULT_MIN_VALUE = 1;
+    public static final boolean DEFAULT_WRAP_SELECTOR_WHEEL = true;
+
+    private final int minValue;
+    private final int maxValue;
+    private final boolean wrapSelectorWheel;
+
+    private NumberPicker picker;
+    private int value;
 
     public NumberPickerPreference(Context context, AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, android.R.attr.dialogPreferenceStyle);
+    }
+
+    public NumberPickerPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        final TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.NumberPickerPreference);
+        minValue = a.getInteger(R.styleable.NumberPickerPreference_minValue, DEFAULT_MIN_VALUE);
+        maxValue = a.getInteger(R.styleable.NumberPickerPreference_maxValue, DEFAULT_MAX_VALUE);
+        wrapSelectorWheel = a.getBoolean(R.styleable.NumberPickerPreference_wrapSelectorWheel, DEFAULT_WRAP_SELECTOR_WHEEL);
+        a.recycle();
     }
 
     @Override
     protected View onCreateDialogView() {
+        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.gravity = Gravity.CENTER;
+
         picker = new NumberPicker(getContext());
-        return (picker);
+        picker.setLayoutParams(layoutParams);
+
+        FrameLayout dialogView = new FrameLayout(getContext());
+        dialogView.addView(picker);
+
+        return dialogView;
     }
 
     @Override
     protected void onBindDialogView(View view) {
         super.onBindDialogView(view);
-        picker.setValue(2);
-        picker.setMaxValue(7);
-        picker.setMinValue(1);
-        picker.setWrapSelectorWheel(false);
+        picker.setMinValue(minValue);
+        picker.setMaxValue(maxValue);
+        picker.setWrapSelectorWheel(wrapSelectorWheel);
+        picker.setValue(getValue());
     }
 
     @Override
     protected void onDialogClosed(boolean positiveResult) {
-        super.onDialogClosed(positiveResult);
-
-        if(positiveResult){
-            persistInt(picker.getValue());
+        if (positiveResult) {
+            picker.clearFocus();
+            int newValue = picker.getValue();
+            if (callChangeListener(newValue)) {
+                setValue(newValue);
+            }
         }
     }
-//
-//    @Override
-//    protected void onSetInitialValue(boolean restorePersistedValue, Object defaultValue) {
-//        int def = (defaultValue instanceof Number) ? (Integer) defaultValue
-//                : ( defaultValue != null ) ? Integer.parseInt(defaultValue.toString()) : 1;
-//        if ( restorePersistedValue ) {
-//            this.initialValue = getPersistedInt(def);
-//        }
-//        else this.initialValue = (Integer) defaultValue;
-//    }
 
     @Override
     protected Object onGetDefaultValue(TypedArray a, int index) {
-        return a.getInt(index, 1);
+        return a.getInt(index, minValue);
+    }
+
+    @Override
+    protected void onSetInitialValue(boolean restorePersistedValue, Object defaultValue) {
+        setValue(restorePersistedValue ? getPersistedInt(minValue) : (Integer) defaultValue);
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+        persistInt(this.value);
+    }
+
+    public int getValue() {
+        return this.value;
     }
 }
