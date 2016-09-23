48ee4cf7ceaad0fb65fb6203340696c1bf99bce4
Robby <robbyl@ovi.com>
2016-09-23 13:11:15 +0300

    Fix start up error for sdk min than 19

--- app/src/main/java/tz/co/wadau/calenderapp/InitialSettingsActivity.java
+++ app/src/main/java/tz/co/wadau/calenderapp/InitialSettingsActivity.java
@@ -3,6 +3,7 @@ package tz.co.wadau.calenderapp;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.TextInputLayout;
@@ -107,8 +108,9 @@ public class InitialSettingsActivity extends AppCompatActivity {
     }
 
     public static boolean isFirstRun(Context context) {
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-//        SharedPreferences sharedPreferences = context.getSharedPreferences("general_settings", Context.MODE_PRIVATE); older versions // TODO: To handle for older devices coz it crashes
+        SharedPreferences sharedPreferences = Build.VERSION.SDK_INT > 19 ?
+                PreferenceManager.getDefaultSharedPreferences(context) :
+                context.getSharedPreferences("general_settings", Context.MODE_PRIVATE);
         return sharedPreferences.getBoolean(InitialSettingsActivity.IS_FIRST_RUN, true);
     }
 
