61b08dd61446878a8b5845bc8193ee35ad41407a
Robby <0881londo@gmail.com>
2016-10-17 11:26:59 +0300

    Creates getStarted function

--- app/src/main/java/tz/co/wadau/calenderapp/InitialSettingsActivity.java
+++ app/src/main/java/tz/co/wadau/calenderapp/InitialSettingsActivity.java
@@ -1,6 +1,5 @@
 package tz.co.wadau.calenderapp;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -10,7 +9,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
 import android.text.TextUtils;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
 import tz.co.wadau.calenderapp.customviews.DatePickerEditText;
@@ -21,24 +19,25 @@ public class InitialSettingsActivity extends AppCompatActivity {
 
     static String IS_FIRST_RUN = "first_run";
     public static String IS_CYCLE_CREATED = "cycle_created";
-
+    public TextInputLayout lastPeriodDateLayout;
+    public TextInputLayout periodDaysLayout;
+    public TextInputLayout cycleDaysLayout;
+    public EditText editLastMensDate;
+    public EditText editPeriodDays;
+    public EditText editCycleDays;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial_settings);
 
-        final Context context = getApplicationContext();
-        final MyCycleDbHelper db = new MyCycleDbHelper(context);
-        final TextInputLayout lastPeriodDateLayout = (TextInputLayout) findViewById(R.id.last_period_date_layout);
-        final TextInputLayout periodDaysLayout = (TextInputLayout) findViewById(R.id.period_days_layout);
-        final TextInputLayout cycleDaysLayout = (TextInputLayout) findViewById(R.id.cycle_days_layout);
+        lastPeriodDateLayout = (TextInputLayout) findViewById(R.id.last_period_date_layout);
+        periodDaysLayout = (TextInputLayout) findViewById(R.id.period_days_layout);
+        cycleDaysLayout = (TextInputLayout) findViewById(R.id.cycle_days_layout);
 
-        final EditText editLastMensDate = lastPeriodDateLayout.getEditText();
-        final EditText editPeriodDays = periodDaysLayout.getEditText();
-        final EditText editCycleDays = cycleDaysLayout.getEditText();
-
-        final Button start = (Button) findViewById(R.id.btn_start);
+        editLastMensDate = lastPeriodDateLayout.getEditText();
+        editPeriodDays = periodDaysLayout.getEditText();
+        editCycleDays = cycleDaysLayout.getEditText();
 
         //Disable soft keyboard so the user can pick date and numbers from the dialog
         editLastMensDate.setInputType(InputType.TYPE_NULL);
@@ -66,41 +65,6 @@ public class InitialSettingsActivity extends AppCompatActivity {
                 newNumberFragment.show(getSupportFragmentManager(), "inputCycleDays");
             }
         });
-
-        start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                checkEmpty(lastPeriodDateLayout);
-                checkEmpty(periodDaysLayout);
-                checkEmpty(cycleDaysLayout);
-
-                final String lastMensDate = editLastMensDate.getText().toString();
-                final String periodDays = editPeriodDays.getText().toString();
-                final String cycleDays = editCycleDays.getText().toString();
-
-                if (!TextUtils.isEmpty(lastMensDate) &&
-                        !TextUtils.isEmpty(periodDays) &&
-                        !TextUtils.isEmpty(cycleDays)) {
-
-                    SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
-                    SharedPreferences.Editor editor = preferences.edit();
-
-                    editor.putString(SettingsFragment.KEY_PREF_LAST_MONTH_MENS_DATE, lastMensDate);
-                    editor.putInt(SettingsFragment.KEY_PREF_MENS_DAYS, Integer.valueOf(periodDays));
-                    editor.putInt(SettingsFragment.KEY_PREF_CYCLE_DAYS, Integer.valueOf(cycleDays));
-                    editor.putBoolean(IS_FIRST_RUN, false);
-                    editor.putBoolean(IS_CYCLE_CREATED, false);
-                    editor.apply();
-
-                    db.addMensCycleDays(context);
-                    startActivity(new Intent(context, CalendarActivity.class));
-                    finish();
-                }
-
-            }
-        });
-
     }
 
     public void showDatePickerDialog(View v) {
@@ -113,6 +77,36 @@ public class InitialSettingsActivity extends AppCompatActivity {
             textInputLayout.setErrorEnabled(true);
             textInputLayout.setError(getString(R.string.this_cannot_be_empty));
             return;
+        }
+    }
+
+    public void getStarted(View v) {
+        checkEmpty(lastPeriodDateLayout);
+        checkEmpty(periodDaysLayout);
+        checkEmpty(cycleDaysLayout);
+
+        final MyCycleDbHelper db = new MyCycleDbHelper(getApplicationContext());
+        final String lastMensDate = editLastMensDate.getText().toString();
+        final String periodDays = editPeriodDays.getText().toString();
+        final String cycleDays = editCycleDays.getText().toString();
+
+        if (!TextUtils.isEmpty(lastMensDate) &&
+                !TextUtils.isEmpty(periodDays) &&
+                !TextUtils.isEmpty(cycleDays)) {
+
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+            SharedPreferences.Editor editor = preferences.edit();
+
+            editor.putString(SettingsFragment.KEY_PREF_LAST_MONTH_MENS_DATE, lastMensDate);
+            editor.putInt(SettingsFragment.KEY_PREF_MENS_DAYS, Integer.valueOf(periodDays));
+            editor.putInt(SettingsFragment.KEY_PREF_CYCLE_DAYS, Integer.valueOf(cycleDays));
+            editor.putBoolean(IS_FIRST_RUN, false);
+            editor.putBoolean(IS_CYCLE_CREATED, false);
+            editor.apply();
+
+            db.addMensCycleDays(getApplicationContext());
+            startActivity(new Intent(getApplicationContext(), CalendarActivity.class));
+            finish();
         }
     }
 }
